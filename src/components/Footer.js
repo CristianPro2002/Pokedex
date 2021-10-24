8b5a3fbe912c9c7e4155d09e5eb9e87b7e58e751
CristianPro2002 <cristian.0713.cs@gmail.com>
2021-10-24 18:50:52 -0500

    avance3

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por <a href="https://www.facebook.com/profile.php?id=100007228694881">Cristian</a>
+    </div>
+    <div>
+      <a href="https://www.facebook.com/profile.php?id=100007228694881">Sigueme en Facebook</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
